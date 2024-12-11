89848648320b2acdd71c6fc987c105d57e3ca376
rafael <rafadevweb@gmail.com>
2024-12-11 16:07:47 -0300

    atualizando aulas

---
+++ Exercicio_2/script.js
@@ -0,0 +1,39 @@
+
+
+
+// function OlaPrimeiroCodigo (){
+//     return "Ola mundo!"
+// }
+
+// let coisa =  OlaPrimeiroCodigo();  
+
+
+
+// console.log("Ola mundo! direto");
+// console.log(OlaPrimeiroCodigo()+" veio por função")
+// console.log(coisa + " veio de uma variavel");
+
+////////////// funçao soma /////////////////////
+
+// let a = 10;
+// let b = 7;
+
+// function soma( num1, num2, num3){
+//     return num1 + num2 + num3 - 10
+// }
+
+// console.log(soma(a,b,58));
+
+////////////// funçao Dobro /////////////////////
+
+function geraDobro(num) {
+    return num+num
+}
+
+geraDobro(1)
+
+
+
+
+
+ 
